d0e92efcf14f038ee549112812c5dcfd4fb2b13b
MatheEduar <edutheu_dev@hotmail.com>
2024-10-04 11:21:08 -0300

    Adicionando estilização e me preparando para utilizar o android studio

--- first_app/App.js
+++ first_app/App.js
@@ -2,14 +2,24 @@ import { StatusBar } from 'expo-status-bar';
 import { LogBox, StyleSheet, Text, View, Button, TextInput } from 'react-native';
 
 export default function App() {
+
+  function goalInputHandler(enteredText) {
+      console.log(enteredText.nativeEvent.text)
+  };
+
+  function addGoalHandler() {
+
+  };
+
+
   return (
     <View style={styles.appContainer}>
       <View style={styles.inputContainer}>
-        <TextInput style={styles.textInput} placeholder='Adicionar Objetivo'/>
-        <Button title="ADD GOAL"/>
+        <TextInput style={styles.textInput} placeholder='Adicionar Objetivo' onChange={goalInputHandler}/>
+        <Button title="ADD GOAL" onPress={addGoalHandler}/>
       </View>    
-      <View>
-        <Text>Lista de Objetivos</Text>  
+      <View style={styles.goalsContainer}>
+        <Text>Lista de Objetivos.</Text>  
       </View>    
     </View>
   );
@@ -17,18 +27,31 @@ export default function App() {
 
 const styles = StyleSheet.create({
   appContainer: {
+    flex: 1,
     padding: 50,
+    paddingHorizontal: 16
   },
+
   inputContainer: {
+    flex: 1,
     flexDirection: "row",
-    justifyContent: "space-between"
+    justifyContent: "space-between",
+    alignItems: "center",
+    marginBottom: 24,
+    borderBottomWidth: 1,
+    borderColor: "#ccc"  
   },
+
   textInput: {
     borderWidth: 1,
     color: "#ccc",
-    width: "80%",
-    marginRight: 5,
-    padding: 10,
+    width: "70%",
+    marginRight: 8,
+    padding: 8,
+  },
+
+  goalsContainer: {
+    flex: 5
   }
   
 });
