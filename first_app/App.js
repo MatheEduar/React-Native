926d9305e655514709b3cfb9da32d6da6317ccf3
MatheEduar <edutheu_dev@hotmail.com>
2024-11-05 18:54:32 -0300

    Adicionando multiplos componentes e adicionando lógica

--- first_app/App.js
+++ first_app/App.js
@@ -1,33 +1,39 @@
 import { StatusBar } from 'expo-status-bar';
 import { useState } from 'react';
-import { LogBox, StyleSheet, Text, View, Button, TextInput } from 'react-native';
+import {
+  StyleSheet,
+  View, 
+  FlatList
+} from 'react-native';
+import GoalItem from './components/GoalItem';
+import GoalInput from './components/GoalInput';
+
 
 export default function App() {
-
-  const [enteredGoalText, setEnteredGoalText] = useState('');
+  
   const [goalList, setGoalList] = useState([]);
 
-  function goalInputHandler(enteredText) {
-      setEnteredGoalText(enteredText.nativeEvent.text)
-  };
-
-  function addGoalHandler() {
-      console.log(enteredGoalText);
-      setGoalList(currentCourseGoals => [...currentCourseGoals, enteredGoalText]);
-      setEnteredGoalText('');
-
+  function addGoalHandler(enteredGoalText) {
+      setGoalList((currentCourseGoals) =>
+        [...currentCourseGoals,
+        {text: enteredGoalText, key: Math.random().toString()},
+        ]);
   };
 
 
   return (
     <View style={styles.appContainer}>
-      <View style={styles.inputContainer}>
-        <TextInput style={styles.textInput} placeholder='Adicionar Objetivo' onChange={goalInputHandler} value={enteredGoalText}/>
-        <Button title="ADD GOAL" onPress={addGoalHandler}/>
-      </View>    
+
+      <GoalInput onAddGoal={addGoalHandler}/>
       <View style={styles.goalsContainer}>
-          {goalList.map((goal) => <Text key={goal}>{goal}</Text>) }
-      </View>    
+        <FlatList data={goalList} renderItem={itemData => {
+          return <GoalItem text={itemData.item.text}/>
+          }
+        }
+        alwaysBounceVertical={false}
+        /> 
+      </View>
+
     </View>
   );
 }
@@ -36,26 +42,10 @@ const styles = StyleSheet.create({
   appContainer: {
     flex: 1,
     padding: 50,
-    paddingHorizontal: 16
+    paddingHorizontal: 16,
+    backgroundColor: '#bae6fd',
   },
 
-  inputContainer: {
-    flex: 1,
-    flexDirection: "row",
-    justifyContent: "space-between",
-    alignItems: "center",
-    marginBottom: 24,
-    borderBottomWidth: 1,
-    borderColor: "#ccc"  
-  },
-
-  textInput: {
-    borderWidth: 1,
-    color: "#ccc",
-    width: "70%",
-    marginRight: 8,
-    padding: 8,
-  },
 
   goalsContainer: {
     flex: 5
