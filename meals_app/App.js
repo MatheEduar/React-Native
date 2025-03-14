693f6b24da82a665c9bb73c348bb8004c28a157c
MatheEduar <edutheu_dev@hotmail.com>
2025-03-14 16:52:51 -0300

    Fazendo a navegação funcionar

--- meals_app/App.js
+++ meals_app/App.js
@@ -1,15 +1,30 @@
+import * as React from 'react';
 import { StyleSheet, View } from 'react-native';
 import { StatusBar } from "expo-status-bar";
 import CategoriesScreen from './screens/CategoriesScreen';
 
+
+import { createNativeStackNavigator } from "@react-navigation/native-stack"
+import { NavigationContainer } from '@react-navigation/native';
+
+
+const Stack = createNativeStackNavigator();
+
+
 export default function App() {
   return (
-  <View style={styles.container}>
-    <CategoriesScreen />
-    <StatusBar style='light'></StatusBar>
-  </View>
+    <>
+      <StatusBar style="light" />
+      <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen name="MealsCategories" component={CategoriesScreen}/>
+        </Stack.Navigator>
+      </NavigationContainer>
+    
+    </>
   );
 }
+
 
 const styles = StyleSheet.create({
   container: {
